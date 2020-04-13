b9a3f3caafc2ccdaa85dbdd8b069366c05f9c64d
David Wickes <dave.wickes@gmail.com>
2020-04-13 22:32:35 +0100

    refactor read tests into groups

--- pkg/gobel/gobel_test.go
+++ pkg/gobel/gobel_test.go
@@ -24,36 +24,70 @@ func TestTokenize(t *testing.T) {
 	}
 }
 
-func TestParse(t *testing.T) {
-	cases := []struct {
-		name    string
-		program string
-		want    interface{}
-	}{
-		{"integer", "1", 1},
-		{"symbol", "symbol", &Symbol{"symbol"}},
-		{"character", `\a`, 'a'},
-		{"slightly more interesting character", `\bel`, '\a'},
-		{"empty list", "()", Nil},
-		{"nil", "nil", Nil},
-		{"one item list", "(1)", &Pair{1, Nil}},
-		{"two item list", "(1 2)", &Pair{1, &Pair{2, Nil}}},
-		{"pair", "(1 . 2)", &Pair{1, 2}},
-		{"three item list", "(1 2 3)", &Pair{1, &Pair{2, &Pair{3, Nil}}}},
-		{"dotted list", "(1 2 . 3)", &Pair{1, &Pair{2, 3}}},
-		{"nested list", "((1))", &Pair{&Pair{1, Nil}, Nil}},
-		{"simplest two lists", "( () () )", &Pair{Nil, &Pair{Nil, Nil}}},
-		{"simplest three lists", "( () () () )", &Pair{Nil, &Pair{Nil, &Pair{Nil, Nil}}}},
-		{"list in second place", "( 1 () )", &Pair{1, &Pair{Nil, Nil}}},
-		{"simple two lists", "( (1) (1) )", &Pair{&Pair{1, Nil}, &Pair{&Pair{1, Nil}, Nil}}},
-		{"two lists", "(+ (+ 1 2) (+ 3 4))",
-			&Pair{&Symbol{"+"}, &Pair{Read("(+ 1 2)")[0], &Pair{Read("(+ 3 4)")[0], Nil}}}},
-		{"funky expression", "(if nil 1 2)", &Pair{&Symbol{"if"}, &Pair{Nil, &Pair{1, &Pair{2, Nil}}}}},
-	}
+type readCase struct {
+	name    string
+	program string
+	want    interface{}
+}
 
+func TestRead(t *testing.T) {
+	t.Run("numbers", func(t *testing.T) {
+		cases := []readCase{
+			{"integer", "1", 1},
+		}
+		testReadCases(cases, t)
+	})
+
+	t.Run("characters", func(t *testing.T) {
+		cases := []readCase{
+			{"a", `\a`, 'a'},
+			{"bel", `\bel`, '\a'},
+		}
+		testReadCases(cases, t)
+	})
+
+	t.Run("symbols", func(t *testing.T) {
+		cases := []readCase{
+			{"symbol", "symbol", &Symbol{"symbol"}},
+		}
+		testReadCases(cases, t)
+	})
+
+	t.Run("lists", func(t *testing.T) {
+		cases := []readCase{
+			{"empty list", "()", Nil},
+			{"nil", "nil", Nil},
+			{"one item list", "(1)", &Pair{1, Nil}},
+			{"two item list", "(1 2)", &Pair{1, &Pair{2, Nil}}},
+			{"pair", "(1 . 2)", &Pair{1, 2}},
+			{"three item list", "(1 2 3)", &Pair{1, &Pair{2, &Pair{3, Nil}}}},
+			{"dotted list", "(1 2 . 3)", &Pair{1, &Pair{2, 3}}},
+			{"nested list", "((1))", &Pair{&Pair{1, Nil}, Nil}},
+			{"simplest two lists", "( () () )", &Pair{Nil, &Pair{Nil, Nil}}},
+			{"simplest three lists", "( () () () )", &Pair{Nil, &Pair{Nil, &Pair{Nil, Nil}}}},
+			{"list in second place", "( 1 () )", &Pair{1, &Pair{Nil, Nil}}},
+			{"simple two lists", "( (1) (1) )", &Pair{&Pair{1, Nil}, &Pair{&Pair{1, Nil}, Nil}}},
+			{"two lists", "(+ (+ 1 2) (+ 3 4))",
+				&Pair{&Symbol{"+"}, &Pair{Read("(+ 1 2)")[0], &Pair{Read("(+ 3 4)")[0], Nil}}}},
+		}
+
+		testReadCases(cases, t)
+	})
+
+	t.Run("misc", func(t *testing.T) {
+		cases := []readCase{
+			{"mixed", "(if nil 1 2)", &Pair{&Symbol{"if"}, &Pair{Nil, &Pair{1, &Pair{2, Nil}}}}},
+		}
+		testReadCases(cases, t)
+	})
+}
+
+func testReadCases(cases []readCase, t *testing.T) {
 	for i := range cases {
 		c := cases[i]
 		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
 			got := Read(c.program)[0]
 			if !reflect.DeepEqual(c.want, got) {
 				t.Errorf("Expected %#v but got %#v", c.want, got)
