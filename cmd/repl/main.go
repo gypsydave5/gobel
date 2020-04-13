b3f513c346c54a7e0d64bf6ed28843dda9bbd414
David Wickes <dave.wickes@gmail.com>
2020-04-13 14:30:45 +0100

    missed the repl main d'oh

---
+++ cmd/repl/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/gypsydave5/gobel/pkg/gobel"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	if isPipe(os.Stdin) {
+		c, _ := ioutil.ReadAll(os.Stdin)
+		result := gobel.Eval(gobel.Parse(string(c)), gobel.DefaultEnv())
+		fmt.Println(result)
+	} else {
+		repl()
+	}
+
+}
+
+func repl() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		expression, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		ts := gobel.Parse(expression)
+		result := gobel.Eval(ts, gobel.DefaultEnv())
+		fmt.Println(result)
+	}
+
+	fmt.Println("\nHave a nice day!")
+}
+
+func isPipe(f *os.File) bool {
+	fi, _ := f.Stat()
+	return (fi.Mode() & os.ModeCharDevice) == 0
+}
