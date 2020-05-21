d08135398a67419fdb61ddaece98d0f26fd78d8d
David Wickes <dave.wickes@gmail.com>
2020-05-21 13:32:37 +0100

    accidentally left these tests commented out

--- pkg/gobel/stringer_test.go
+++ pkg/gobel/stringer_test.go
@@ -31,12 +31,12 @@ func TestStringer(t *testing.T) {
 		}{
 			{"nil", nil, "()"},
 			{"Nil", g.Nil, "()"},
-			// {"simple pair", &g.Pair{1, 2}, "(1 . 2)"},
-			// {"simple proper list", &g.Pair{1, g.Nil}, "(1)"},
-			// {"two item proper list", &g.Pair{1, &g.Pair{2, g.Nil}}, "(1 2)"},
-			// {"three item proper list", g.Read("(1 2 3)")[0].(*g.Pair), "(1 2 3)"},
-			// {"nested lists", g.Read("((1) (2 (3)))")[0].(*g.Pair), "((1) (2 (3)))"},
-			// {"dotted list", &g.Pair{1, &g.Pair{2, 3}}, "(1 2 . 3)"},
+			{"simple pair", &g.Pair{1, 2}, "(1 . 2)"},
+			{"simple proper list", &g.Pair{1, g.Nil}, "(1)"},
+			{"two item proper list", &g.Pair{1, &g.Pair{2, g.Nil}}, "(1 2)"},
+			{"three item proper list", g.Read("(1 2 3)")[0].(*g.Pair), "(1 2 3)"},
+			{"nested lists", g.Read("((1) (2 (3)))")[0].(*g.Pair), "((1) (2 (3)))"},
+			{"dotted list", &g.Pair{1, &g.Pair{2, 3}}, "(1 2 . 3)"},
 		}
 
 		for _, c := range cases {
